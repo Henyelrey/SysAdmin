74672cadea4c73bc21b7fd62940cb30d3ec93b2b
Henyelrey <henyel91@gmail.com>
2025-10-15 16:13:36 -0500

    cambios

--- SysAlmacen/src/main/java/pe/edu/upeu/sysalmacen/SysAlmacenApplication.java
+++ SysAlmacen/src/main/java/pe/edu/upeu/sysalmacen/SysAlmacenApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SysAlmacenApplication {
 
+
+	//probando
 	public static void main(String[] args) {
 		SpringApplication.run(SysAlmacenApplication.class, args);
 	}
